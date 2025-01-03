4374597fb69a697fdc476440446e5336666606ff
lucashthiele <lucasthiele0@outlook.com>
2025-01-03 12:58:07 -0300

    fix: fixed import with old directory name

--- cmd/routes.go
+++ cmd/routes.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
-	"github.com/lucashthiele/doc-it/controller/page"
+	"github.com/lucashthiele/doc-it/handler/page"
 )
 
 func SetupRoutes(e *echo.Echo) {
 	indexHandler := page.IndexHandler{}
 
+	// pages
 	e.GET("/", indexHandler.Show)
 }
