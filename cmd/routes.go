4b68886c87d3be133a6006ea3f64c9037bb4ebb1
lucashthiele <lucasthiele0@outlook.com>
2025-01-09 13:10:58 -0300

    starting route

--- cmd/routes.go
+++ cmd/routes.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/lucashthiele/doc-it/handler/api"
 	"github.com/lucashthiele/doc-it/handler/page"
 )
 
 func SetupRoutes(e *echo.Echo) {
-	indexHandler := page.IndexHandler{}
-	loginHandler := page.LoginHandler{}
 
 	// static assets
 	e.Static("/public", "view/assets")
 
 	// pages
+	indexHandler := page.IndexHandler{}
+	loginHandler := page.LoginHandler{}
+
 	e.GET("/", indexHandler.Show)
 	e.GET("/login", loginHandler.Show)
+
+	// api
+	githubHandler := api.GithubHandler{}
+
+	e.POST("/github", githubHandler.Post)
 }
---
+++ handler/api/github.go
@@ -0,0 +1,10 @@
+package api
+
+import "github.com/labstack/echo/v4"
+
+type GithubHandler struct {
+}
+
+func (gh GithubHandler) Post(c echo.Context) error {
+	return nil
+}
