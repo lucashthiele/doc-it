ed9a1a16ee6d0b91b8cfbc69a723e5e424478fe5
lucashthiele <lucasthiele0@outlook.com>
2025-01-10 12:59:43 -0300

    feat: added CORS config

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.CORS())
 
 	SetupRoutes(e)
 
