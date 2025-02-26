33b8e6352ccc229106c396a71ae4e718755e16ad
lucashthiele <lucasthiele0@outlook.com>
2025-02-26 13:29:34 -0300

    chore: removed comments

--- internal/handlers/github.go
+++ internal/handlers/github.go
@@ -16,20 +16,19 @@ type (
 	}
 )
 
-// Handles the request, validates it, call services, build response
 func (gh *GitHub) Post(c echo.Context) error {
 	githubRequest := new(GitHubRequest)
 	if err := c.Bind(githubRequest); err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err) // todo - improve response
+		return echo.NewHTTPError(http.StatusBadRequest, err)
 	}
 
 	if err := c.Validate(githubRequest); err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err) // todo - improve response
+		return echo.NewHTTPError(http.StatusBadRequest, err)
 	}
 
 	user, err := services.SaveGithubUser(githubRequest.Code)
 	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err) // todo - improve response
+		return echo.NewHTTPError(http.StatusBadRequest, err)
 	}
 
 	return c.JSON(http.StatusOK, user)
